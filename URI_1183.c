efe4dff9501acf58ae1fcb269ef5be2e14ae293d
Thiago Almeida <thilemos18@gmail.com>
2020-10-13 21:35:32 -0300

    Update URI_1183.c

--- URI_1183.c
+++ URI_1183.c
@@ -1,3 +1,10 @@
+/*  
+
+Leia um caractere maiúsculo, que indica uma operação que deve ser realizada e uma matriz M[12][12]. 
+Em seguida, calcule e mostre a soma ou a média considerando somente aqueles elementos que estão acima da diagonal principal da matriz.
+
+*/
+
 #include <stdio.h>
 
 int main(void) {
