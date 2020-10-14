1ad741ab6812f395b103986fa9258999a6e01f4e
Thiago Almeida <thilemos18@gmail.com>
2020-10-13 21:46:05 -0300

    Update URI_1578.c

--- URI_1578.c
+++ URI_1578.c
@@ -1,3 +1,23 @@
+/*
+
+Entrada
+A primeira linha da entrada é constituída por um único inteiro positivo N (N ≤ 100), o qual designa o número de matrizes cujos “quadrados” ainda não foram calculados. 
+Em seguida ocorre a descrição de cada uma das N matrizes. 
+A primeira linha da descrição de uma matriz consiste de um único inteiro M (1 ≤ M ≤ 20), o qual representa o número de linhas e o número de colunas da matriz. 
+Seguem, então, M linhas, cada uma com M inteiros aij (0 ≤ aij ≤ 232-1, 1 ≤ i,j ≤ M), os quais correspondem às células da matriz, 
+de modo que valores consecutivos numa mesma linha são separados por um espaço em branco.
+
+Saída
+Imprima o “quadrado” de cada matriz da entrada, conforme o que Atrapalhilton entende pelo “quadrado” de uma matriz. 
+Antes de imprimir cada “quadrado”, imprima a linha “Quadrado da matriz #x:” (sem as aspas), 
+para ajudar Atrapalhilton a não se perder na hora de passar a limpo os resultados para o caderno. 
+Comece a contagem em x = 4, afinal, Atrapalhilton já calculou os “quadrados” das 3 primeiras matrizes. 
+Adicione tantos espaços em branco à esquerda de cada valor quanto necessários para que os valores de uma mesma coluna fiquem todos alinhados à direita, 
+de modo que haja ao menos um valor em cada coluna não precedido por espaços em branco além do espaço em branco obrigatório que separa colunas consecutivas. 
+Imprima também uma linha em branco entre “quadrados” de matrizes consecutivas.
+
+*/
+
 #include <stdio.h>
 #include <math.h>
 
