6bb45e5b4a010cbacb2483d8e8e66884a4d04890
Thiago Almeida <thilemos18@gmail.com>
2020-10-13 21:38:12 -0300

    Update URI_1285.c

--- URI_1285.c
+++ URI_1285.c
@@ -1,3 +1,15 @@
+/*
+
+Os habitantes de Nlogônia são muito supersticiosos. Uma de suas crenças é que os números das casas de rua que têm um dígito repetido traz sorte ruim para os moradores. 
+Portanto, eles nunca iriam viver em uma casa que tem o número 838 ou 1004, por exemplo.
+
+A rainha de Nlogônia ordenou a construção de uma nova avenida à beira-mar e quer atribuir para as novas casas apenas números sem dígitos repetidos, 
+para evitar desconforto entre os seus súditos. 
+Você foi nomeado por Sua Majestade para escrever um programa que, dado dois inteiros N e M, determine a quantidade máxima possível de casas que podem assumir um número 
+entre N e M inclusive, sem que ocorram dígitos repetidos nestes números.
+
+*/
+
 #include <stdio.h>
 
     int ultimodigito(int N){
