a5f642636644940976b81ffc01dfb47bc840a8a9
Thiago Almeida <thilemos18@gmail.com>
2020-10-13 21:29:23 -0300

    Update URI_1160.c

--- URI_1160.c
+++ URI_1160.c
@@ -1,3 +1,19 @@
+/* 
+
+Entrada
+A primeira linha da entrada contém um único inteiro T, indicando o número de casos de teste (1 ≤ T ≤ 3000). 
+Cada caso de teste contém 4 números: dois inteiros PA e PB (100 ≤ PA < 1000000, PA < PB ≤ 1000000) indicando respectivamente a população de A e B,
+e dois valores G1 e G2 (0.1 ≤ G1 ≤ 10.0, 0.0 ≤ G2 ≤ 10.0, G2 < G1) com um digito após o ponto decimal cada,
+indicando respectivamente o crescimento populacional de A e B (em percentual).
+
+Saída
+Imprima, para cada caso de teste, quantos anos levará para que a cidade A ultrapasse a cidade B em número de habitantes.
+Obs.: se o tempo for mais do que 100 anos o programa deve apresentar a mensagem: Mais de 1 seculo.
+Neste caso, acredito que seja melhor interromper o programa imediatamente após passar de 100 anos, 
+caso contrário você poderá receber como resposta da submissão deste problema "Time Limit Exceeded".
+
+*\
+
 #include <stdio.h>
 
  
